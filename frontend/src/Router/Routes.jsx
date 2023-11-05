c6e65b161324fb2d93c55ba0f674f011dc54e725
Sk Shariful Islam <98546214+shariful10@users.noreply.github.com>
2023-11-05 22:26:05 +0600

    Implemented Style Active Link

--- frontend/src/Router/Routes.jsx
+++ frontend/src/Router/Routes.jsx
@@ -1,6 +1,8 @@
 import { createBrowserRouter } from "react-router-dom";
 import App from "../App";
 import Home from "../pages/Home";
+import About from "../pages/About";
+import Dashboard from "../pages/Dashboard";
 
 const router = createBrowserRouter([
 	{
@@ -10,6 +12,14 @@ const router = createBrowserRouter([
 			{
 				path: "/",
 				element: <Home />,
+			},
+			{
+				path: "/about",
+				element: <About />,
+			},
+			{
+				path: "/dashboard",
+				element: <Dashboard />,
 			},
 		],
 	},
--- frontend/src/components/shared/Navbar.jsx
+++ frontend/src/components/shared/Navbar.jsx
@@ -1,26 +1,74 @@
-import { Link } from "react-router-dom";
+import { Link, useLocation } from "react-router-dom";
 import logo from "../../assets/images/logo.svg";
+import { useEffect, useState } from "react";
 
 const Navbar = () => {
+	const [active, setActive] = useState(null); // Initialize active state as null
+	const location = useLocation();
+
+	// Update active state when location changes
+	useEffect(() => {
+		if (location.pathname === "/") {
+			setActive("home");
+		} else if (location.pathname === "/about") {
+			setActive("about");
+		} else if (location.pathname === "/dashboard") {
+			setActive("dashboard");
+		} else {
+			setActive(null);
+		}
+	}, [location]);
+
 	return (
 		<nav className="flex justify-between items-center w-5/6 mx-auto py-4">
 			<Link to="/">
 				<img src={logo} width={200} alt="" />
 			</Link>
 			<ul className="flex justify-center items-center gap-4">
-				<li className="font-medium group">
-					<Link to="/">Home</Link>
-					<span className="block w-0 group-hover:w-full transition-all duration-700 h-1 bg-[#ff6575] mt-[1px] rounded-full" />
-				</li>
-				<li className="font-medium group">
-					<Link to="/about">
-						About
-						<span className="block w-0 group-hover:w-full transition-all duration-700 h-1 bg-[#ff6575] mt-[1px] rounded-full" />
+				<li
+					onClick={() => setActive("home")}
+					className={`font-medium group ${
+						active === "home" ? "active" : "default"
+					}`}
+				>
+					<Link to="/">
+						Home
+						<span
+							className={`${
+								active === "home" && "w-full"
+							} block w-0 group-hover:w-full transition-all duration-700 h-1 bg-[#ff6575] mt-[1px] rounded-full`}
+						/>
 					</Link>
 				</li>
-				<li className="font-medium group">
-					<Link to="/dashboard">Dashboard</Link>
-					<span className="block w-0 group-hover:w-full transition-all duration-700 h-1 bg-[#ff6575] mt-[1px] rounded-full" />
+				<li
+					onClick={() => setActive("about")}
+					className={`font-medium group ${
+						active === "about" ? "active" : "default"
+					}`}
+				>
+					<Link to="/about">
+						About
+						<span
+							className={`${
+								active === "about" && "w-full"
+							} block w-0 group-hover:w-full transition-all duration-700 h-1 bg-[#ff6575] mt-[1px] rounded-full`}
+						/>
+					</Link>
+				</li>
+				<li
+					onClick={() => setActive("dashboard")}
+					className={`font-medium group ${
+						active === "dashboard" ? "active" : "default"
+					}`}
+				>
+					<Link to="/dashboard">
+						Dashboard
+						<span
+							className={`${
+								active === "dashboard" && "w-full"
+							} block w-0 group-hover:w-full transition-all duration-700 h-1 bg-[#ff6575] mt-[1px] rounded-full`}
+						/>
+					</Link>
 				</li>
 			</ul>
 			<div className="">
