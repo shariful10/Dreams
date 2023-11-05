c3c63140a7a4c972c2a7fa738b0e75aa6ea04904
Sk Shariful Islam <98546214+shariful10@users.noreply.github.com>
2023-11-05 23:58:06 +0600

    Mobile Menu Implemented

--- frontend/src/components/shared/Navbar.jsx
+++ frontend/src/components/shared/Navbar.jsx
@@ -1,12 +1,15 @@
 import { Link, useLocation } from "react-router-dom";
 import logo from "../../assets/images/logo.svg";
 import { useEffect, useState } from "react";
+import { Bars3Icon, XMarkIcon } from "@heroicons/react/24/solid";
 
 const Navbar = () => {
-	const [active, setActive] = useState(null); // Initialize active state as null
 	const location = useLocation();
+	const [active, setActive] = useState(null);
+	const [isMenuToggle, setIsMenuToggle] = useState(false);
 
-	// Update active state when location changes
+	const isAboveMediumScreens = useMediaQuery("(min-width: 1060px)");
+
 	useEffect(() => {
 		if (location.pathname === "/") {
 			setActive("home");
@@ -24,58 +27,128 @@ const Navbar = () => {
 			<Link to="/">
 				<img src={logo} width={200} alt="" />
 			</Link>
-			<ul className="flex justify-center items-center gap-4">
-				<li
-					onClick={() => setActive("home")}
-					className={`font-medium group ${
-						active === "home" ? "active" : "default"
-					}`}
+			{isAboveMediumScreens ? (
+				<>
+					<ul className="flex justify-center items-center gap-4">
+						<li
+							onClick={() => setActive("home")}
+							className={`font-medium group ${
+								active === "home" ? "active" : "default"
+							}`}
+						>
+							<Link to="/">
+								Home
+								<span
+									className={`${
+										active === "home" && "w-full"
+									} block w-0 group-hover:w-full transition-all duration-700 h-1 bg-[#ff6575] mt-[1px] rounded-full`}
+								/>
+							</Link>
+						</li>
+						<li
+							onClick={() => setActive("about")}
+							className={`font-medium group ${
+								active === "about" ? "active" : "default"
+							}`}
+						>
+							<Link to="/about">
+								About
+								<span
+									className={`${
+										active === "about" && "w-full"
+									} block w-0 group-hover:w-full transition-all duration-700 h-1 bg-[#ff6575] mt-[1px] rounded-full`}
+								/>
+							</Link>
+						</li>
+						<li
+							onClick={() => setActive("dashboard")}
+							className={`font-medium group ${
+								active === "dashboard" ? "active" : "default"
+							}`}
+						>
+							<Link to="/dashboard">
+								Dashboard
+								<span
+									className={`${
+										active === "dashboard" && "w-full"
+									} block w-0 group-hover:w-full transition-all duration-700 h-1 bg-[#ff6575] mt-[1px] rounded-full`}
+								/>
+							</Link>
+						</li>
+					</ul>
+					<div className="">
+						<button className="px-4 py-2 bg-[#ff6575] hover:bg-[#e72f41] rounded-md text-white font-medium transition-all duration-300">
+							Login
+						</button>
+					</div>
+				</>
+			) : (
+				<button
+					className="rounded-full bg-[#ff6575] hover:bg-[#e72f41] p-2"
+					onClick={() => setIsMenuToggle(!isMenuToggle)}
 				>
-					<Link to="/">
-						Home
-						<span
-							className={`${
-								active === "home" && "w-full"
-							} block w-0 group-hover:w-full transition-all duration-700 h-1 bg-[#ff6575] mt-[1px] rounded-full`}
-						/>
-					</Link>
-				</li>
-				<li
-					onClick={() => setActive("about")}
-					className={`font-medium group ${
-						active === "about" ? "active" : "default"
-					}`}
-				>
-					<Link to="/about">
-						About
-						<span
-							className={`${
-								active === "about" && "w-full"
-							} block w-0 group-hover:w-full transition-all duration-700 h-1 bg-[#ff6575] mt-[1px] rounded-full`}
-						/>
-					</Link>
-				</li>
-				<li
-					onClick={() => setActive("dashboard")}
-					className={`font-medium group ${
-						active === "dashboard" ? "active" : "default"
-					}`}
-				>
-					<Link to="/dashboard">
-						Dashboard
-						<span
-							className={`${
-								active === "dashboard" && "w-full"
-							} block w-0 group-hover:w-full transition-all duration-700 h-1 bg-[#ff6575] mt-[1px] rounded-full`}
-						/>
-					</Link>
-				</li>
-			</ul>
-			<div className="">
-				<button className="px-4 py-2 bg-[#ff6575] hover:bg-[#e72f41] rounded-md text-white font-medium transition-all duration-300">
-					Login
+					<Bars3Icon className="h-6 w-6 text-white" />
 				</button>
-			</div>
+			)}
+			{/* <==<<=== Mobile Menu ===>>==> */}
+			{!isAboveMediumScreens && isMenuToggle && (
+				<div className="fixed right-0 bottom-0 z-40 h-full w-[300px] bg-primary-100 shadow-2xl">
+					{/* <==<<=== Close Icon ===>>==> */}
+					<div className="flex justify-end px-12 pb-12 pt-7">
+						<button onClick={() => setIsMenuToggle(!isMenuToggle)}>
+							<XMarkIcon className="h-8 w-8 text-gray-400" />
+						</button>
+					</div>
+					{/* <==<<=== Menu Items ===>>==> */}
+					<div className="ml-[33%] flex flex-col gap-10 text-2xl">
+						<li
+							onClick={() => setActive("home")}
+							className={`font-medium group ${
+								active === "home" ? "active" : "default"
+							}`}
+						>
+							<Link to="/">
+								Home
+								<span
+									className={`${
+										active === "home" && "w-full"
+									} block w-0 group-hover:w-full transition-all duration-700 h-1 bg-[#ff6575] mt-[1px] rounded-full`}
+								/>
+							</Link>
+						</li>
+						<li
+							onClick={() => setActive("about")}
+							className={`font-medium group ${
+								active === "about" ? "active" : "default"
+							}`}
+						>
+							<Link to="/about">
+								About
+								<span
+									className={`${
+										active === "about" && "w-full"
+									} block w-0 group-hover:w-full transition-all duration-700 h-1 bg-[#ff6575] mt-[1px] rounded-full`}
+								/>
+							</Link>
+						</li>
+						<li
+							onClick={() => setActive("dashboard")}
+							className={`font-medium group ${
+								active === "dashboard" ? "active" : "default"
+							}`}
+						>
+							<Link to="/dashboard">
+								Dashboard
+								<span
+									className={`${
+										active === "dashboard" && "w-full"
+									} block w-0 group-hover:w-full transition-all duration-700 h-1 bg-[#ff6575] mt-[1px] rounded-full`}
+								/>
+							</Link>
+						</li>
+					</div>
+				</div>
+			)}
 		</nav>
 	);
 };
